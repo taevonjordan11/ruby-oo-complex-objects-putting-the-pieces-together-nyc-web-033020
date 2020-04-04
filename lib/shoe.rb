36558259d63eea1f23f234556743a320cbe0f631
Taevon Jordan <nude.plus@Taevons-MacBook-Pro.local>
2020-04-04 15:02:42 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,7 @@
 class Shoe
+
+  attr_accessor :condition
+
   def initialize(brand)
     @brand = brand
   end
@@ -10,38 +13,29 @@ class Shoe
   def color=(color)
     @color = color
   end
-
   def color
     @color
   end
 
-def size=(size)
-  @size = size
-end
+  def size=(size)
+    @size = size
+  end
+  def size
+    @size
+  end
 
-def size
-  @size
-end
+  def material=(material)
+    @material = material
+  end
+  def material
+    @material
+  end
 
-def material=(material)
-  @material = material
-end
-
-def material
-  @material
-end
-
-def condition=(condition)
-  @condition = condition
-end
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 
 
-def condition
-  @condition
-end
-
-def cobble
-  puts "Your shoe is as good as new!"
-end
 
 end
